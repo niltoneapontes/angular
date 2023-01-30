6ac64e036d1f40ddf28d20ec636bfdec1075961b
Nilton <niltoneapontes@gmail.com>
2023-01-30 12:09:51 -0300

    feat: create login structure

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  username=""
+  password=""
+  errorMessage=""
 
+  constructor(private router: Router, private auth: AuthService) {}
+
+  ngOnInit(): void {
+
+  }
+
+  goToSignUp(): void {
+    this.router.navigate(["signup"])
+  }
+
+  login() {
+    if(this.username.trim().length === 0) {
+      this.errorMessage = "Please enter an username"
+    } else if(this.password.trim().length === 0) {
+      this.errorMessage = "Please enter an username"
+    } else {
+      this.errorMessage = ""
+      let res = this.auth.login(this.username, this.password)
+      if (res === 200) {
+        this.router.navigate(["home"])
+      } else {
+        this.errorMessage = "Invalid user or password"
+      }
+    }
+  }
 }
